fd3d3eed31aa2aedf6cc662341facbd1f4ee8991
Sadman Rafith <108008599+SadmanRafith@users.noreply.github.com>
2023-09-28 00:11:36 +0600

    Add files via upload

---
+++ Array/Task 1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Task_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number of elements in the array: ");
+            int n= int.Parse(Console.ReadLine());
+
+            int[] num= new int[n];
+
+            Console.WriteLine("Enter the elements of the array: ");
+
+            for(int i=0; i<n; i++)
+            {
+                num[i] = int.Parse(Console.ReadLine());
+            }
+
+            int small = num[0];
+            int large = num[0];
+
+            foreach(int i in num)
+            {
+                if(i<small)
+                {
+                    small = i;
+                }
+
+                if (i > large)
+                {
+                    large = i;
+                }
+            }
+            Console.WriteLine($"Largest Number: {large}");
+            Console.WriteLine($"Smallest Number: {small}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Array/Task 3/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace lab1task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] arr1 = new int[3, 3];
+            int[,] arr2 = new int[3, 3];
+            int[,] arr3 = new int[3, 3];
+            Console.WriteLine("Enter the elements of the first array = ");
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    arr1[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+            Console.WriteLine("Enter the elements of the second array = ");
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    arr2[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    arr3[i, j] = arr1[i, j] + arr2[i, j];
+                }
+            }
+            Console.WriteLine("Addition of two matrix = ");
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.WriteLine(arr3[i, j]);
+                }
+            }
+        }
+    }
+}
