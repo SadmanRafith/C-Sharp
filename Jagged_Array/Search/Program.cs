e8993fa5992459fa74413214d8b2064faa0b9c53
Sadman Rafith <108008599+SadmanRafith@users.noreply.github.com>
2023-10-02 21:37:05 +0600

    Add files via upload

---
+++ Jagged_Array/Search/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] jagged_arr = new int[3][]
+                {
+             new int[3] {1, 2, 3},
+             new int[3] {11, 34, 67},
+             new int[3] {89, 23,4}
+                };
+            int count= 0;
+
+            Console.WriteLine("Enter the element to be searched: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < jagged_arr.Length; i++) 
+            { 
+               for (int j = 0; j<jagged_arr[i].Length; j++)
+                {
+                    if (jagged_arr[i][j] == n)
+                    {
+                        Console.WriteLine($"The searched is found at row{i}, column{j}");
+                        count++;
+                    }
+                }
+            }
+            if(count==0)
+            {
+                Console.WriteLine("The searched element not found");
+            }
+        }
+    }
+}
