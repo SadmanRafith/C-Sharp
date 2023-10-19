f0de50af0b734975092bdf2f522207935632fc5d
Sadman Rafith <108008599+SadmanRafith@users.noreply.github.com>
2023-10-19 12:33:41 +0600

    Add files via upload

---
+++ Inheritance/Bird/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+class Bird1
+{
+    public bool fly;
+
+    public Bird1(bool fly)
+    {
+        this.fly = fly;
+    }
+
+    public int canfly(bool fly)
+    {
+        if (fly == true)
+        {
+            return 1;
+        }
+        else
+        {
+            return 0;
+        }
+
+    }
+}
+
+class penguin : Bird1
+{
+    public string color;
+
+    public penguin(string color, bool fly) : base(fly)
+    {
+        this.color = color;
+        canfly(fly);
+    }
+    public void display_penguin()
+    {
+        Console.WriteLine("Penguine Information:");
+        Console.WriteLine("Color: " + color);
+        if (canfly(fly) == 1)
+        {
+            Console.WriteLine("Bird can fly");
+        }
+        else
+        {
+            Console.WriteLine("Bird can't fly");
+        }
+    }
+}
+
+class robin : Bird1
+{
+    public string color;
+
+    public robin(string color, bool fly) : base(fly)
+    {
+        this.color = color;
+        canfly(fly);
+    }
+    public void display_robin()
+    {
+        Console.WriteLine("robin Information:");
+        Console.WriteLine("Color: " + color);
+        if (canfly(fly) == 1)
+        {
+            Console.WriteLine("Bird can fly");
+        }
+        else
+        {
+            Console.WriteLine("Bird can't fly");
+        }
+    }
+}
+
+
+namespace Bird
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            penguin p1 = new penguin("White & Black", false);
+            p1.display_penguin();
+            Console.WriteLine();
+
+            robin r1 = new robin("Blue", true); 
+            r1.display_robin();
+
+        }
+    }
+}
---
+++ Inheritance/First Class/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Net.NetworkInformation;
+
+class Human
+{
+    protected string Organ;
+
+    public Human(string organ)
+    {
+        this.Organ = organ;
+    }
+}
+
+class Student : Human
+{
+    public int id;
+    public float cgpa;
+
+    public Student(int id, float cgpa, string organ):base(organ)
+    {
+        this.id = id;
+        this.cgpa = cgpa;
+    }
+    public void display()
+    {
+        Console.WriteLine("Student Id: " + id);
+        Console.WriteLine("Cgpa: " + cgpa);
+        Console.WriteLine("Organ: " + Organ);
+    }
+}
+
+class ExStudent : Student
+{
+    public string Year;
+
+    public ExStudent(int id, float cgpa, string year, string organ) : base(id,cgpa,organ)
+    {
+       this.Year = year;
+    }
+
+    public void display_ex_Student()
+    {
+        Console.WriteLine("Student Id: " + id);
+        Console.WriteLine("Cgpa: " + cgpa);
+        Console.WriteLine("Graduated Year: "+Year);
+        Console.WriteLine("Organ: " + Organ);
+    }
+}
+
+class Teacher : Human
+{
+    public string name;
+    public string dept;
+
+    public Teacher(string name, string dept, string organ) : base(organ)
+    {
+        this.name = name;
+        this.dept = dept;
+    }
+
+    public void teacher_display()
+    {
+        Console.WriteLine("Teacher Name: " + name);
+        Console.WriteLine("Department: " + dept);
+        Console.WriteLine("Organ: " + Organ);
+    }
+}
+
+
+namespace First_class
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s1 = new Student(02, 3.5f, "Heart");
+            s1.display();
+            Console.WriteLine();
+
+            ExStudent e1 = new ExStudent(03, 3.75f, "2020", "Heart");
+            e1.display_ex_Student();
+            Console.WriteLine();
+
+            Teacher t1 = new Teacher("Kabir", "CSE", "Liver");
+            t1.teacher_display();
+        }
+    }
+}
